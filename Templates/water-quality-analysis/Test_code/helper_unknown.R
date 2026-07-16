2f67141918ba242bf2b2fbb32ca760efe1799051
enussdorfer <nussdore@oregonstate.edu>
2026-07-16 13:36:04 -0700

    update

--- Templates/water-quality-analysis/Test_code/helper_unknown.R
+++ Templates/water-quality-analysis/Test_code/helper_unknown.R
@@ -1,3 +1,10 @@
+library(dplyr)
+library(stringr)
+library(lubridate)
+library(writexl)
+
+
+
 # Functions to safely write slsxs and csvs without overwriting anything
 safe.write_xlsx <- function(x, path, ...) {
   if(file.exists(path)) {
@@ -32,37 +39,52 @@ safe.write_csv <- function(x, path, ...) {
 
 #functions for data labels 
 
-#function 1  
+
+sn <- sum(nsamp) #total sample amount from the vector that was filled out
+
+
+
+#first function   
 data_label <- function(){
   sampleinfo <- data.frame(site=site,nsamp=nsamp,interval=interval,study=study,samp_type=samp_type,blanktime=blanktime, sn=sn, rn=rn)
   data_info <- function(sampleinfo){ #this function creates 
     return(data.frame(
-      site =rep(sampleinfo$site, times = sampleinfo$nsamp),
-      blank_time=sampleinfo$blanktime,
-      botnum = sprintf("%02d",rep(1:24, times= ceiling(sampleinfo$nsamp/24))[1:sampleinfo$nsamp]), study=sampleinfo$study,samp_type=sampleinfo$samp_type
+      site =rep(sampleinfo$site, times = sampleinfo$nsamp), #repeats site for number of samples per site
+      blank_time=sampleinfo$blanktime, #inserts blank time format 
+      botnum = sprintf("%02d",rep(1:24, times= ceiling(sampleinfo$nsamp/24))[1:sampleinfo$nsamp]), #bottle numbers 1-24 repeated for number of samples
+      study=sampleinfo$study,samp_type=sampleinfo$samp_type
     ))}
-  x<-lapply(1:nrow(sampleinfo),function(x){data_info(sampleinfo[x,])})%>%
+  
+  labels<-lapply(1:nrow(sampleinfo),function(x){data_info(sampleinfo[x,])})%>% #using data_info function to make data.frame
     bind_rows()%>%
     mutate(
-      randomn=sample(rn:sn,sn, replace=F),
-      sample_name = paste0(study, "_", "R", sprintf("%04d",randomn)),
+      randomn=sample(rn:sn,sn, replace=F), #add random numbers
+      sample_name = paste0(study, "_", "R", sprintf("%04d",randomn)), 
       field_sample_ID_blank= paste0(study, "_", site, "_", samp_type,"_",blanktime))
+  
+  
+  return(labels)
+  # return(safe.write_csv(labels,
+  #                       path = "blank_ISCO_example.csv",
+  #                       row.names = FALSE)
+         # )
 }
 
-#second version
+#second function
 
 lab_label <- function(){
-  sampletimeinfo <- data.frame(nsamp=nsamp,interval=interval,start=start,DOC=DOC,DOM=DOM,CCAL=CCAL,Levoglucosan=Levoglucosan,Excess=Excess)   
+  sampletimeinfo <- data.frame(nsamp=nsamp,interval=interval,updated_start=updated_start,DOC=DOC,DOM=DOM,CCAL=CCAL,Levoglucosan=Levoglucosan,Excess=Excess)   
   blank_labels <- data.frame(read.csv(blank_fileformat))
+  
   site_label_blank <- function(sampletimeinfo){
-    timestart <- as.POSIXct(sampletimeinfo$start)
-    timelist <- seq(timestart, by=paste0(sampletimeinfo$interval, "hours"), length = sampletimeinfo$nsamp)
+    timestart <- as.POSIXct(sampletimeinfo$updated_start)
+    timelist <- seq(timestart, by=paste0(sampletimeinfo$interval, " hours"), length = sampletimeinfo$nsamp)
     return(
       data.frame(time_PST=as.character(timelist),datetime_PST=format(timelist,"%Y%m%d%_%H%M")))}
 
   analysis_opt <- c("CN", "AQ", "NU", "LV", "EX")
   analysis_choice <- c(DOC, DOM, CCAL, Levoglucosan, Excess)
-  analysiscodes <- analysis_opt[analysis_choice]
+  analysiscodes <- analysis_opt[analysis_choice] #analytes used
   analysisn <- length(analysiscodes) #how many analytes there are
   datacodes<- data.frame(code = rep(analysiscodes,times=sn)) %>% #add labcodes
     mutate(analysis = case_when(
@@ -75,13 +97,13 @@ lab_label <- function(){
   
   data_labels <- lapply(1:nrow(sampletimeinfo),function(x){site_label_blank(sampletimeinfo[x,])}) %>% #makes the frame
     bind_rows()%>%
-    mutate(blank_labels)%>%
+    mutate(blank_labels)%>% #add on your ddata frame that had blank labeles 
     mutate(field_sample_ID= paste0(study, "_", site, "_",samp_type,"_",datetime_PST))%>%
-    arrange(randomn)%>%
-    slice(rep(1:n(), each = analysisn))%>%
-    mutate(datacodes)
+    arrange(randomn)%>% #sort by random number
+    slice(rep(1:n(), each = analysisn))%>% #multiply for each analyte
+    mutate(datacodes) #add the data codes 
   
-  return(data_labels)
+  return(data_labels) #add print here 
   
 }
 
